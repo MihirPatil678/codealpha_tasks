ec49a725efbf8e82d30f566225bee77423c3c0dc
MihirPatil678 <mihirpatil678@gmail.com>
2025-12-23 17:28:09 +0530

    Add files via upload

--- CodeAlpha_Calculator_Program/CodeAlpha_Calculator_Program.c
+++ CodeAlpha_Calculator_Program/CodeAlpha_Calculator_Program.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '/':
+            result = num1 / num2;
+            printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        default:
+            printf("Error: Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
